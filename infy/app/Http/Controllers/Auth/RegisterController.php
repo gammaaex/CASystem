d5e6cd5ab19ca6a75103386e20141cdbf25b275e
鈴木 光 <>
2018-06-20 03:45:28 +0900

    登録機能を実装

--- infy/app/Http/Controllers/Auth/RegisterController.php
+++ infy/app/Http/Controllers/Auth/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\Authority;
 use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
@@ -37,19 +38,25 @@ class RegisterController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest');
+        $this->middleware('web');
+    }
+
+    public function showRegistrationForm()
+    {
+        $authorities = Authority::pluck('name', 'id');
+
+        return view('auth.register', compact('authorities'));
     }
 
     /**
      * Get a validator for an incoming registration request.
      *
-     * @param  array  $data
+     * @param  array $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
@@ -58,13 +65,13 @@ class RegisterController extends Controller
     /**
      * Create a new user instance after a valid registration.
      *
-     * @param  array  $data
-     * @return \App\User
+     * @param  array $data
+     * @return \App\Models\User
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
+        return \App\Models\User::create([
+            'authority_id' => $data['authority'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
